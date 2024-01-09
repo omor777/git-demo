57d98e870f5c75f63627b188e50e3eebd5de4e43
omor777 <omor0843@gmail.com>
2024-01-09 14:21:20 +0600

    added version 6 and 7 also 7.1

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log("version 2");
 console.log('version 3');
 console.log('version4');
 console.log('version 5');
+console.log('version 6');
+console.log('version 7');
+console.log('version 7.1');
+console.log('version 7.2');
